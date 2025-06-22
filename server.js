78081d00528b0761d541e1c62af49a15277439e6
Tomás Antunes <tomasantunes@gmail.com>
2025-06-22 13:30:27 +0100

    Move API key to config file

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const axios = require('axios');
+var secretConfig = require('./secret-config.json');
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/news/us', async (req, res) => {
-  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=XXXXXXXX');
+  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + secretConfig.API_KEY);
   res.json(response.data.articles);
 });
 
 app.get('/news/pt', async (req, res) => {
-  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=pt&apiKey=XXXXXXXX');
+  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=pt&apiKey=' + secretConfig.API_KEY);
   res.json(response.data.articles);
 });
 
 app.get('/news/tech', async (req, res) => {
-  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=XXXXXXXX');
+  const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=' + secretConfig.API_KEY);
   res.json(response.data.articles);
 });
 
